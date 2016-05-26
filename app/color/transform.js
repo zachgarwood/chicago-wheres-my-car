1b9409ba6f8dd34e89c493a84794ada80cf725d3
Zach Garwood <zachgarwood@gmail.com>
2016-05-25 22:08:05 -0500

    Added color model attribute transformer

---
+++ app/color/transform.js
@@ -0,0 +1,42 @@
+import Transform from 'ember-data/transform';
+import _object from 'lodash/object';
+
+const COLOR_CODES = {
+  'BGE': 'beige',
+  'BLK': 'black',
+  'BLU': 'blue',
+  'BRO': 'brown',
+  'BRZ': 'bronze',
+  'CAM': 'camouflage',
+  'COM': 'chrome',
+  'CPR': 'copper',
+  'CRM': 'cream',
+  'DBL': 'dark blue',
+  'DGR': 'dark green',
+  'GLD': 'gold',
+  'GRN': 'green',
+  'GRY': 'grey',
+  'LBL': 'light blue',
+  'LGR': 'light green',
+  'MAR': 'maroon',
+  'ONG': 'orange',
+  'PNK': 'pink',
+  'RED': 'red',
+  'SIL': 'silver',
+  'TAN': 'tan',
+  'TEA': 'teal',
+  'TPE': 'taupe',
+  'TRQ': 'turquoise',
+  'WHI': 'white',
+  'YEL': 'yellow',
+};
+
+export default Transform.extend({
+  deserialize(serialized) {
+    return (serialized in COLOR_CODES) ?  COLOR_CODES[serialized] : serialized;
+  },
+
+  serialize(deserialized) {
+    return _object.findKey(COLOR_CODES, deserialized);
+  }
+});
--- app/vehicle/model.js
+++ app/vehicle/model.js
@@ -2,7 +2,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
 export default Model.extend({
-  color: attr(),
+  color: attr('color'),
   make: attr(),
   plate: attr(),
   state: attr(),
