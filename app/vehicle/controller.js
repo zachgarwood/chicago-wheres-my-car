7adb690fee43f670fb2ec9a000c386c0ac920ce6
Zach Garwood <zachgarwood@gmail.com>
2016-06-03 11:52:54 -0500

    WIP adding filter selects

--- app/vehicle/controller.js
+++ app/vehicle/controller.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Controller.extend({
-  sortedVehicles: Ember.computed.sort('model', 'sortDefinition'),
-  sortDefinition: ['date:desc', 'licensePlate'],
+  queryParams: [ 'color', 'last_seen', 'make' ],
+  last_seen: moment().subtract(1, 'days').startOf('day').toISOString(), 
+  color: null,
+  make: null,
+
+  today: moment().startOf('day').toISOString(),
+  yesterday: moment().subtract(1, 'days').startOf('day').toISOString(), 
+  daysAgo: moment().subtract(6, 'days').startOf('day').toISOString(), 
+  weeksAgo: moment().subtract(4, 'weeks').startOf('week').toISOString(),
+  monthsAgo: moment().subtract(2, 'months').startOf('month').toISOString(),
+  aLongTimeAgo: moment().subtract(1, 'years').startOf('year').toISOString(),
 });
--- app/vehicle/route.js
+++ app/vehicle/route.js
@@ -1,10 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+  queryParams: {
+    color: { refreshModel: true },
+    last_seen: { refreshModel: true },
+    make: { refreshModel: true },
+  },
+  model(params) {
+    let query = {
+      where: [ `relocated_date > "${params.last_seen}"` ],
+      order: [ 'relocated_date desc' ],
+    };
+    if (params.color) {
+      query.where.push('color = "' + params.color + '"');
+    }
+    if (params.make) {
+      query.where.push('make = "' + params.make + '"');
+    }
     return Ember.RSVP.hash({
-      relocatedVehicles: this.store.findAll('relocated-vehicle'),
-      towedVehicles: this.store.findAll('towed-vehicle'),
+      relocatedVehicles: this.store.query('relocated-vehicle', query),
+      towedVehicles: [], //this.store.findAll('towed-vehicle'),
     }).then(function(hash) {
       let allVehicles = Ember.A();
       allVehicles.pushObjects(hash.relocatedVehicles.toArray());
