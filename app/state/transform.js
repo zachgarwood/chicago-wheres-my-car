f6c76ebd9c28f927dc3c19b605d0efe7b96bb559
Zach Garwood <zachgarwood@gmail.com>
2016-06-04 15:31:49 -0500

    Updated state transform

--- app/state/transform.js
+++ app/state/transform.js
@@ -1,67 +1,14 @@
+import STATE_CODES from './codes';
 import Transform from 'ember-data/transform';
 import _object from 'lodash/object';
 
-// Source:
-// http://statetable.com/
-const STATE_CODES = {
-  "Alabama": "AL",
-  "Alaska": "AK",
-  "Arizona": "AZ",
-  "Arkansas": "AR",
-  "California": "CA",
-  "Colorado": "CO",
-  "Connecticut": "CT",
-  "Delaware": "DE",
-  "Florida": "FL",
-  "Georgia": "GA",
-  "Hawaii": "HI",
-  "Idaho": "ID",
-  "Illinois": "IL",
-  "Indiana": "IN",
-  "Iowa": "IA",
-  "Kansas": "KS",
-  "Kentucky": "KY",
-  "Louisiana": "LA",
-  "Maine": "ME",
-  "Maryland": "MD",
-  "Massachusetts": "MA",
-  "Michigan": "MI",
-  "Minnesota": "MN",
-  "Mississippi": "MS",
-  "Missouri": "MO",
-  "Montana": "MT",
-  "Nebraska": "NE",
-  "Nevada": "NV",
-  "New Hampshire": "NH",
-  "New Jersey": "NJ",
-  "New Mexico": "NM",
-  "New York": "NY",
-  "North Carolina": "NC",
-  "North Dakota": "ND",
-  "Ohio": "OH",
-  "Oklahoma": "OK",
-  "Oregon": "OR",
-  "Pennsylvania": "PA",
-  "Rhode Island": "RI",
-  "South Carolina": "SC",
-  "South Dakota": "SD",
-  "Tennessee": "TN",
-  "Texas": "TX",
-  "Utah": "UT",
-  "Vermont": "VT",
-  "Virginia": "VA",
-  "Washington": "WA",
-  "West Virginia": "WV",
-  "Wisconsin": "WI",
-  "Wyoming": "WY",
-};
-
 export default Transform.extend({
   deserialize(serialized) {
-    return (serialized in STATE_CODES) ?  STATE_CODES[serialized] : serialized;
+    let stateValues = _object.invert(STATE_CODES);
+    return (serialized in stateValues) ? stateValues[serialized] : serialized;
   },
 
   serialize(deserialized) {
-    return _object.findKey(STATE_CODES, deserialized);
+    return (deserialized in STATE_CODES) ? STATE_CODES[deserialized] : deserialized;
   }
 });
